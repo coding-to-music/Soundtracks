ab06d6817528748b1ab851576ab135f98e535658
trilambda122 <trilambda122>
2021-01-12 16:52:06 -0600

    resolved issue# 8 and issue #7

--- client/src/components/Youtube/YoutubeItemList.js
+++ client/src/components/Youtube/YoutubeItemList.js
@@ -4,6 +4,7 @@ import SoundtrackContext from '../../context/soundtrackContext'
 import api from '../../utils/api'
 
 export default function YoutubeItemList(props) {
+  const  parser = new DOMParser;
   const {setYoutubeVideo, youtubeVideo }= useContext(SoundtrackContext)
   const history = useHistory()  
 
@@ -14,17 +15,30 @@ export default function YoutubeItemList(props) {
   // }
 
   const srcString = `//www.youtube.com/embed/${props.youtube.id.videoId}`
-  console.log(srcString)
+  
+  // need to parse out unicode elements from what youtube has returned 
+  const t1 = parser.parseFromString(props.youtube.snippet.title, "text/html")
+  const title=t1.body.textContent
+
+  const date = new Date(props.youtube.snippet.publishTime).toLocaleDateString(
+    'en-us',
+    {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    }
+  );
   return (
     <div>
+      {console.log(props.youtube)}
     <li className="list-group-item">
       <div className='row'>
         <div className='col-4'>
-          <iframe title="video" width="280" height="160" src={srcString} frameborder="0" allow="accelerometer; autoplay;"/>
+          <iframe title="video"  src={srcString} frameborder="1" marginwidth="10" allow="accelerometer; autoplay; fullscreen" allowfullscreen/>
           </div>
       
-      <div className='col'><p className='text-capitalize display-6'>{props.youtube.snippet.title} </p>
-      <h4 className='text-capitalize fw-light'> {props.youtube.snippet.publishTime}</h4>
+      <div className='col'><p className='text-capitalize display-6'>{title} </p>
+      <h4 className='text-capitalize fw-light'> {date}</h4>
       <p className='text-capitalize fw-light'>{props.youtube.snippet.description}</p>
       </div>
      
@@ -37,5 +51,5 @@ export default function YoutubeItemList(props) {
 
 
 // {props.youtube.snippet.title}
-
+// width="280" height="160"
 {/* <iframe width="480" height="360" src="//www.youtube.com/embed/N8I-hnSS7DQ" frameborder="0" allow="accelerometer; autoplay;"/> */}
