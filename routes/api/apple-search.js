b3256fc191dc77e10b23b51429c64e61f3664e90
trilambda122 <trilambda122>
2021-01-20 10:03:07 -0600

    search api working

--- routes/api/apple-search.js
+++ routes/api/apple-search.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
 const router = express.Router();
 
 
@@ -11,7 +12,7 @@ const alg = process.env.ALG
 console.log('teamid, ', teamId)
 console.log('keyid, ', keyId)
 console.log("alg, ", alg)
-// setup json payload 
+// setup json payload for the jwt signing
 payload = {
   "iss": teamId,
   "exp": Math.floor((Date.now() / 1000)+43200),
@@ -19,6 +20,23 @@ payload = {
 }
 
 
+// FUNCTIONS
+async function getSongResults(searchStr,token){
+  console.log('token: ', token)
+  console.log('search string: ', searchStr)
+  const url = `https://api.music.apple.com/v1/catalog/us/search?term=${searchStr}&limit=1&types=songs`
+  const headers = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }}
+  
+  const response = await axios.get(url,headers);
+  console.log('response: ', response)
+  return response
+}
+  
+
+// ROUTES 
 router.get("/api/applesearch", (req, res) => {
   res.send('WELCOME THIS API FUNCTION DOES NOT HAVE A GET RESPONSE, PLEASE USE A POST METHOD');
 });
@@ -26,7 +44,9 @@ router.get("/api/applesearch", (req, res) => {
 router.post('/api/applesearch', ({body},res) => {
   const privateKey = process.env.SECRET_KEY
   const devToken = jwt.sign(payload, privateKey, { algorithm: alg, header:{kid:keyId}});
-  res.send('under construction');
+  const result = getSongResults(body.searchString,devToken).then((result)=>{
+    res.json(result.data)
+    })
   })
 
   module.exports = router;
