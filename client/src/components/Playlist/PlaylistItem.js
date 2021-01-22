1e2335b4560788628ed9656b72e8ba17a2d76dee
trilambda122 <trilambda122>
2021-01-22 06:42:54 -0600

    apple search wokring, with albumn artwork and release date

--- client/src/components/Playlist/PlaylistItem.js
+++ client/src/components/Playlist/PlaylistItem.js
@@ -2,11 +2,21 @@ import React from 'react'
 
 export default function PlaylistItem(props) {
 
-
-
 let artwork=props.song.results.songs.data[0].attributes.artwork.url
 artwork = artwork.replace("{w}","250")
 artwork =artwork.replace("{h}","250")
+
+
+const date = new Date(props.song.results.songs.data[0].attributes.releaseDate).toLocaleDateString(
+  'en-us',
+  {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }
+);
+
+
 
 console.log('ARTWORK, ', artwork)
   return (
@@ -17,6 +27,7 @@ console.log('ARTWORK, ', artwork)
       
       <div className='col'><p className='text-capitalize display-6'>{props.song.results.songs.data[0].attributes.name} </p>
       <h5 className='text-capitalize fw-light'>{props.song.results.songs.data[0].attributes.artistName}</h5>
+      <h5 className='text-capitalize fw-light'>{date}</h5>
       </div>
      
       </div>
